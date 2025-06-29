6d9b2caa25dc47fec636de1c7d101af544f6ea8d
SyBen <tieuxuyen1234@gmail.com>
2025-06-29 21:27:58 +0700

    Sửa bien

--- Startup.cs
+++ Startup.cs
@@ -120,7 +120,7 @@ namespace WebApi
 
             // Global CORS policy
             app.UseCors(x => x
-                .WithOrigins("tour-fe-dr6e.vercel.app")
+                .WithOrigins("tour-fe-mu.vercel.app")
                 .AllowAnyMethod()
                 .AllowAnyHeader());
 
